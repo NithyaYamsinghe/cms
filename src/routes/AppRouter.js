125c918ba8b73113ebc12074c181e4e54917d871
NithyaYamsinghe <nitheeromeshi@gmail.com>
2021-06-28 22:29:45 +0530

    added home screen page

--- src/routes/AppRouter.js
+++ src/routes/AppRouter.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import SignInScreen from "./../pages/SignInScreen";
+import HomeScreen from "./../pages/HomeScreen";
 
 const AppRouter = () => {
   return (
     <React.Fragment>
       <Switch>
-        <Route exact path="/" component={SignInScreen}></Route>
+        <Route exact path="/" component={HomeScreen}></Route>
+        <Route exact path="/sign-in" component={SignInScreen}></Route>
       </Switch>
     </React.Fragment>
   );
